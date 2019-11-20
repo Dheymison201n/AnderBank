cb468397352aae67d42bf819711f70a81a4dfb15
Alysson-UFC <51142101+Alysson-UFC@users.noreply.github.com>
2019-11-20 20:49:09 -0300

    Add files via upload

--- AnderBank/src/anderbank/AnderBank.java
+++ AnderBank/src/anderbank/AnderBank.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package anderbank;
 
 import java.io.IOException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
